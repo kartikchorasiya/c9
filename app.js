cfbc2786b01e9607072da38df086a280df2cbc58
Kartik Chorasiya <kartikchorasiya1996@gmail.com>
2017-06-22 06:02:10 +0000

    blog app

---
+++ app.js
@@ -0,0 +1,124 @@
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var express = require("express");
+var expressSanitizer = require("express-sanitizer");
+
+var methodOverride = require("method-override");
+var app=express();
+mongoose.connect("mongodb://localhost/restful_blog_app");
+mongoose.Promise = global.Promise;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(expressSanitizer());                //It must be afterwards app.use(bodyParser)
+
+app.use(methodOverride("_method"));             //However, it can be anything but mostly seen this and also we're gonna follow a particular convention
+
+var blogSchema = new mongoose.Schema({
+    title:String,
+    image:String,
+    body:String,
+    created:{type:Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//REStful Routes
+
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+//INDEX Route
+app.get("/blogs", function(req, res){
+    Blog.find({},function(err, blogs){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("index", {blogs:blogs});
+        }
+    });
+});
+
+//NEW Route
+
+app.get("/blogs/new", function(req, res){
+   res.render("new"); 
+});
+
+//CREATE Route
+
+app.post("/blogs", function(req, res){
+    
+    console.log(req.body);
+    
+    req.body.blog.body = req.sanitize(req.body.blog.body);  //This will sanitize the body of blog, i.e., will remove the script tag from the body of the blog if any so that no one can place any malicious code
+    
+    console.log("=======");
+    
+    console.log(req.body);
+    //create blog and then redirect
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        }else{
+            res.redirect("/blogs");
+        }
+        
+    });
+});
+
+//SHOW Route
+
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("show", {blog:foundBlog});
+        }
+});
+});
+
+//EDIT Route
+
+app.get("/blogs/:id/edit", function(req, res){                   //Edit is like a combination of new and show
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.render("edit", {blog:foundBlog});
+        }
+    });
+});
+    
+//UPDATE ROUTE
+
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){                      //(id, newdata, callback function)
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs/"+req.params.id);
+        }
+    });
+});
+
+//DELETE Route
+
+app.delete("/blogs/:id", function(req, res){
+    //destroy blog and redirect somewhere
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        }else{
+            res.redirect("/blogs");
+        }
+    });
+});
+app.listen(process.env.PORT, process.env.IP);
